7041a52eabc517a16a650571ab9ebf0752936f38
Vinod Kumar <vinodkumar@192.168.1.2>
2025-10-02 19:58:25 +0530

    Initial Commit

---
+++ src/main/java/com/codingkiddo/SpringReactiveJwtSampleApplication.java
@@ -0,0 +1,13 @@
+package com.codingkiddo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveJwtSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveJwtSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingkiddo/SpringReactiveJwtSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingkiddo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveJwtSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
