3b0118e22c324e3ca021e4c9a84dc24c9f0a7d19
Vinod Kumar <vinodkumar@192.168.1.5>
2025-10-03 17:52:07 +0530

    modified:   pom.xml 	modified:   src/main/java/com/codingkiddo/SpringReactiveJwtSampleApplication.java 	modified:   src/test/java/com/codingkiddo/SpringReactiveJwtSampleApplicationTests.java

--- src/main/java/com/codingkiddo/SpringReactiveJwtSampleApplication.java
+++ src/main/java/com/codingkiddo/SpringReactiveJwtSampleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringReactiveJwtSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringReactiveJwtSampleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveJwtSampleApplication.class, args);
+    }
 }
